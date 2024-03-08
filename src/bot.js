e324cfadbc2630f4c031ec14c65830bace043be0
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-08 06:22:38 +0000

    ✨ tweak: update message for mentioned team member

--- src/bot.js
+++ src/bot.js
@@ -543,7 +543,7 @@ client.on("messageCreate", async (message) => {
 		let mentioned = message.guild.members.cache.get(mention.users.first().id)
 		if (mentioned.roles.cache.hasAny(...roleIDs)) {
 			message.reply({
-				embeds: [sendEmbedMessage(`We have moved to a community driven thirdweb support model.\n\nYou can ask me all things thirdweb in the <#${ASKAI_CHANNEL}> channel. Use the command \`!askai\` or \`!ask\` followed by your question to get started.`)],
+				embeds: [sendEmbedMessage(`We have move to a community driven discord support model.\n\nYou can ask me all things thirdweb in the <#${ASKAI_CHANNEL}> channel. Use the command \`!askai\` or \`!ask\` followed by your question to get started.`)],
 			});
 		}
 	}
