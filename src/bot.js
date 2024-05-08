00b4ce53efa308c7e8b5b1fd00609be85c45709d
Waren Gonzaga <opensource@warengonzaga.com>
2024-05-08 05:16:21 +0000

    ✨ tweak: move the interaction logic to events file

--- src/bot.js
+++ src/bot.js
@@ -114,60 +114,9 @@ client.on("messageCreate", async (message) => {
 });
 
 //listen to button clicks
-client.on("interactionCreate", async (interaction) => {
-	let messageId = interaction.message.id
-
-	// check if the interaction is a button click
-	if (interaction.isButton()) {
-
-		if (interaction.customId === "helpful") {
-			await interaction.reply({
-				embeds: [sendEmbedMessage(`Thank you so much for your feedback!`)],
-				content: `🔔 <@${interaction.user.id}>`,
-				ephemeral: true,
-			});
-			await redis.get(messageId, async (err, result) => {
-				if (err) {
-					console.error(err);
-				} else {
-					await context.setQueryFeedback({
-						queryId: result,
-						helpful: true,
-					});
-				}
-			});
-			await interaction.message.edit({ components: [] });
-			await redis.del(messageId);
-
-			// log the feedback
-			console.log(`[${serverTime()}][log]: User sent a "Helpful" feedback!`);
-		}
-		
-		if (interaction.customId === "not-helpful") {
-			await interaction.reply({
-				embeds: [sendEmbedMessage(`Thank you for your valuable feedback, this will help us improve the responses of our AI assistant.\n\nIn the meantime, would you like to contact a human customer success agent? Just click the link or the button below to submit a ticket.`)],
-				content: `🔔 <@${interaction.user.id}>`,
-				ephemeral: true,
-				components: [CloseButtonComponent()],
-			});
-			await redis.get(messageId, async (err, result) => {
-				if (err) {
-					console.error(err);
-				} else {
-					await context.setQueryFeedback({
-						queryId: result,
-						helpful: false,
-					});
-				}
-			});
-			await interaction.message.edit({ components: [] });
-			await redis.del(messageId);
-
-			// log the feedback
-			console.log(`[${serverTime()}][log]: User sent a "Not Helpful" feedback!`);
-		}
-	}
-});
+// client.on("interactionCreate", async (interaction) => {
+	
+// });
 
 /**
  * Don't modify or update the code below.
